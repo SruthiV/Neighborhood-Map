8623abaf05a8f6d740a85086559aab32db8ca7d2
Sruthi Vinukonda <vinukonda.sruthi@gmail.com>
2018-04-24 13:45:26 -0500

    Add marker data

---
+++ js/data.js
@@ -0,0 +1,22 @@
+var locations = [
+    {
+        title: "Be Raw",
+        location: {lat: 32.865052, lng: -96.80816}
+    },
+    {
+        title: "D'Vegan",
+        location: {lat:  32.921719, lng: -96.7195}
+    },
+    {
+        title: "V-Eats",
+        location: {lat:  32.779304, lng: -96.828045}
+    },
+    {
+        title: "Goji Cafe",
+        location: {lat:  32.895171, lng: -96.900733}
+    },
+    {
+        title: "Sundown at Granada",
+        location: {lat:  32.830658, lng: -96.769917}
+    }
+]
